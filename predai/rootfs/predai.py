1cc4295ae32a511ad01191fc1540d72913f0c38e
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2024-04-06 13:23:02 +0100

    Update predai.py

--- predai/rootfs/predai.py
+++ predai/rootfs/predai.py
@@ -4,11 +4,41 @@ from typing import Any
 import pandas as pd
 from datetime import datetime, timedelta
 from neuralprophet import NeuralProphet, set_log_level
+import os
+import aiohttp
+import websocket
+import asyncio
+import json
+import ssl
 
 #  - torch
 #  - neuralprophet==0.8.0
 
 TIME_FORMAT_OCTOPUS = "%Y-%m-%dT%H:%M:%S%z"
+
+class HAInterface():
+    def __init__(self):
+        self.ha_key = os.environ.get("SUPERVISOR_TOKEN")
+        self.ha_url = "http://supervisor/core"
+        print("HA Interface started key {} url {}".format(self.ha_key, self.ha_url))
+
+    async def create_websocket(self):
+            # change to websocket protocol
+            url = self.ha_url
+            if url.startswith("https://"):
+                url = url.replace("https", "wss", 1)
+            elif url.startswith("http://"):
+                url = url.replace("http", "ws", 1)
+
+            # ssl options
+            sslopt = {"cert_reqs": ssl.CERT_NONE}
+            ws = websocket.create_connection("{}/api/websocket".format(url), sslopt=sslopt)
+
+            # wait for successful connection
+            res = ws.recv()
+            result = json.loads(res)
+            print("Connected to HA {}".format(result))
+            self.ws = ws
 
 class Prophet:
     def __init__(self):
@@ -34,5 +64,16 @@ class Prophet:
         # Get the prediction for a specific time
         return 0
 
-test = Prophet()
-print("Here")
+async def main():
+    print("Here")
+    print("environ {}".format(os.environ))
+    print("Args {}".format(os.sys.argv))
+    interface = HAInterface()
+    await interface.create_websocket()
+
+    print("Here2")
+    test = Prophet()
+    print("Here3")
+
+asyncio.run(main())
+print("Test finished")
