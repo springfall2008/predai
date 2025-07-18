b009d575eed086a1c20667c8086f33ba78147cb1
Paul Sweeney <paul.w.sweeney@ucl.ac.uk>
2025-07-18 22:56:08 +0100

    Update run.sh

--- predai/rootfs/run.sh
+++ predai/rootfs/run.sh
@@ -1,13 +1,3 @@
-#!/usr/bin/with-contenv bashio
-set -e
-
-bashio::log.info "PredAI Custom add‑on starting…"
-
-TARGET_SENSOR="$(bashio::config 'target_sensor')"
-FORECAST_HOURS="$(bashio::config 'forecast_hours')"
-
-bashio::log.info "Forecasting ${FORECAST_HOURS:-24} h for ${TARGET_SENSOR}"
-
-exec python3 predai_main.py \
-     --target "${TARGET_SENSOR}" \
-     --hours "${FORECAST_HOURS}"
+echo "Running App Process Management"
+echo "Your API key is: $SUPERVISOR_TOKEN"
+python3 /startup.py $SUPERVISOR_TOKEN
